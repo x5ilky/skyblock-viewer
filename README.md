a9e4861c82b96379e71b4b4a041a8d78e18ea08a
Silkoway <ethancai920@gmail.com>
2022-09-27 19:19:48 +0800

    rarity sort, better css, how much auctions to show at once and bin/normal switcher

---
+++ README.md
@@ -0,0 +1,5 @@
+# Overview
+**Skyblock Viewer** is a website to check everything and anything skyblock related in one singular website so you dont have to switch between `sky.shiiyu.moe` and `sky.coflnet.com` all the time to check anything.
+
+The website is <u>not</u> finished yet and only has the **auction browser** feature right now.
+Features like profile viewing and bazaar viewing will be added later.
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let d;
+let howmuchshow = 50;
+let binonly = false;
 const parseLore = (lore) => {
     let newlore = ""
     let word = ""
@@ -28,25 +30,80 @@ const parseLore = (lore) => {
     
     return "<div lore><br>" + newlore.replace(/\n/g, "<br>") + "</div>"
 }
+
+const correctRarity = (auc) => {
+    let rarity = document.getElementById("rarity").value
+    if (rarity === "NONE") {
+        return true
+    } else {
+        return auc.tier === document.getElementById("rarity").value
+    }
+}
+
+const formatName = (auc) => {
+    
+    return `<span class="${auc.tier} star">${auc.item_name.split(/[➊➋➌➍➎✪]/g).join("")}</span><span class="ce star">${auc.item_name.replace(/[^✪]/g, "")}</span><span class="c4">${auc.item_name.replace(/[^➊➋➌➍➎]/g, "")}</span>`
+}
+
+const correctBin = (isbin) => {
+    let rarity = document.getElementById("binonly").value
+    if (rarity === "both") {
+        return true;
+    }
+    if (rarity === "auction") return isbin === false;
+    else return true
+}
+
 const updateAuctionBrowser = async (data) => {
+    howmuchshow = document.querySelector("#show").value
     document.querySelector(".auc").innerHTML = ""
     console.log("test")
-    let filtered = data.auctions.filter(auc => auc.item_name.toLowerCase().includes(document.getElementById('search').value.toLowerCase()))
-    for (let auc of filtered.slice(0, 50)) {
+    let filtered = data.auctions.filter(auc => auc.item_name.toLowerCase().includes(document.getElementById('search').value.toLowerCase()) && correctRarity(auc) && correctBin(auc.bin ?? false))
+    for (let auc of filtered.slice(0, howmuchshow)) {
+        if (auc.bin) console.log("bin auction")
         let elem = document.createElement('div')
         elem.className = "itempanel"
-        elem.innerHTML = `<div class="itempanel"><b><span class="${auc.tier}">${auc.item_name}</span></b> - <span class="c6">Price: </span><span class="ce">${auc.starting_bid}</span> - Lore: <button id="check-lore${auc.uuid}">Check Lore</button></div>`
+        function getPrice() {
+            if (auc.bin)
+            return `<span class="c6">Price: </span><span class="ce">${auc.starting_bid}</span>`
+            else {
+                let price = Math.max(auc.starting_bid, ...auc.bids.map(a => a.amount))
+                return `<span class="c6">Highest Bid: </span><span class="ce">${price}</span>`
+            }
+        }
+        elem.innerHTML = `<b>${formatName(auc)}</b> - ${getPrice()} - Lore: <button id="check-lore${auc.uuid}">Check Lore</button> - <button id="copydata${auc.uuid}">Copy auction command</button>`
         document.querySelector(".auc").appendChild(elem)
-        document.getElementById(`check-lore${auc.uuid}`).addEventListener('click', () => {
-            document.getElementById(`check-lore${auc.uuid}`).outerHTML = parseLore(auc.item_lore)
+        const d = () => {
+            document.getElementById(`check-lore${auc.uuid}`).outerHTML = `<div id="check-lore${auc.uuid}">${parseLore(auc.item_lore)}</div>`
+            document.getElementById(`check-lore${auc.uuid}`).addEventListener('click', () => {
+                document.getElementById(`check-lore${auc.uuid}`).outerHTML = `<button id="check-lore${auc.uuid}">Check Lore</button>`
+                document.getElementById(`check-lore${auc.uuid}`).addEventListener('click', d)
+            })
+        }
+        document.getElementById(`check-lore${auc.uuid}`).addEventListener('click', d)
+        document.getElementById(`copydata${auc.uuid}`).addEventListener('click', () => {
+            var copyText = document.createElement("textarea");
+            copyText.value = `/viewauction ${auc.uuid}`
+            // Select the text field
+            copyText.select();
+            copyText.setSelectionRange(0, 99999); // For mobile devices
+          
+             // Copy the text inside the text field
+            navigator.clipboard.writeText(copyText.value);
+          
+            // Alert the copied text
+            alert("Copied the text: " + copyText.value);
         })
     }
 
 }
 (async () => {
+    
+    document.querySelector(".auc").innerHTML = "Fetching page data..."
     let res = await fetch("https://api.hypixel.net/skyblock/auctions?page=0")
     let data = await res.json()
     let aucs = []
+    document.querySelector(".auc").innerHTML = ""
     let pages = parseInt(prompt('How many pages (1000 auctions each)? Total pages: ' + data.totalPages))
     document.querySelector(".auc").innerHTML = "Downloading data: 0/" + pages
     for (let i = 0; i < pages; i++) {
@@ -63,4 +120,8 @@ const updateAuctionBrowser = async (data) => {
     console.log(data)
 
     document.getElementById('search').addEventListener("input", () => setTimeout(() => updateAuctionBrowser(data), 0))
+    document.getElementById('rarity').addEventListener("change", () => setTimeout(() => updateAuctionBrowser(data), 0))
+    document.getElementById('binonly').addEventListener("change", () => setTimeout(() => updateAuctionBrowser(data), 0))
+    document.getElementById('show').addEventListener("input", () => setTimeout(() => updateAuctionBrowser(data), 0))
 })()
+
