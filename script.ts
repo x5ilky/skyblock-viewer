a1eead26f3131b8491403aed1e5576a5e8951b0d
Silkoway <ethancai920@gmail.com>
2022-10-12 10:20:37 +0800

    bug fixes

--- script.ts
+++ script.ts
@@ -129,8 +129,8 @@ const updateAuctionBrowser = (data: AuctionPageResponse, reload = false) => {
         && correctLore(auc)
         && correctStars(auc)
         && ((auc.extradata?.hot_potato_count?.value ?? 0) === parseInt($$("#hotpotato").value) || $$("#hotpotato").value === "any")
-        && ((auc.extradata?.art_of_war_count?.value ?? 0) === ($$("#artofwar").checked ? 1 : 0))
-        && ((auc.extradata?.rarity_upgrades?.value ?? 0) === ($$("#recom").checked ? 1 : 0))
+        && (((auc.extradata?.art_of_war_count?.value ?? 0) === ($$("#artofwar").value === "true" ? 1 : 0)) || ($$("#artofwar").value === "any"))
+        && (((auc.extradata?.rarity_upgrades?.value ?? 0) === ($$("#recom").value === "true" ? 1 : 0)) || ($$("#recom").value === "any"))
     )
     filtered = sortAuctions(filtered) ?? []
     $$(".loaded").textContent = `${filtered.slice(0, howmuchshow).length} out of ${filtered.length}`
